cd5a2bb1039d9f20e0ac263952280985c431e5af
yeyuanzheng177 <391606040@qq.com>
2018-10-31 16:25:01 +0800

    Update receptive_field.py
    receptive_fields_dilated-convolution

--- receptive_field.py
+++ receptive_field.py
@@ -1,23 +1,11 @@
 from math import *
 net_struct = {
-    # 'alexnet': {'net': [[11, 4, 0], [3, 2, 0], [5, 1, 2], [3, 2, 0], [3, 1, 1], [3, 1, 1], [3, 1, 1], [3, 2, 0]],
-    #             'name': ['conv1', 'pool1', 'conv2', 'pool2', 'conv3', 'conv4', 'conv5', 'pool5']},
-    # 'vgg16': {'net': [[3, 1, 1], [3, 1, 1], [2, 2, 0], [3, 1, 1], [3, 1, 1], [2, 2, 0], [3, 1, 1], [3, 1, 1], [3, 1, 1],
-    #                   [2, 2, 0], [3, 1, 1], [3, 1, 1], [3, 1, 1], [2, 2, 0], [3, 1, 1], [3, 1, 1], [3, 1, 1],
-    #                   [2, 2, 0]],
-    #           'name': ['conv1_1', 'conv1_2', 'pool1', 'conv2_1', 'conv2_2', 'pool2', 'conv3_1', 'conv3_2',
-    #                    'conv3_3', 'pool3', 'conv4_1', 'conv4_2', 'conv4_3', 'pool4', 'conv5_1', 'conv5_2', 'conv5_3',
-    #                    'pool5']},
-    # 'zf-5': {'net': [[7, 2, 3], [3, 2, 1], [5, 2, 2], [3, 2, 1], [3, 1, 1], [3, 1, 1], [3, 1, 1]],
-    #          'name': ['conv1', 'pool1', 'conv2', 'pool2', 'conv3', 'conv4', 'conv5']},
+    
     'network': {'net': [[3, 1, 0,0], [3, 1, 0,0], [3, 1, 0,5]],
-             'name': ['conv1', 'pool1', 'conv2']}
+             'name': ['conv1', 'conv2', 'dilate_convolution']}
 }
 
 imsize = 224
-
-
-
 def outFromIn(isz, net, layernum):
     totstride = 1
     insize = isz
@@ -30,13 +18,6 @@ def outFromIn(isz, net, layernum):
         totstride = totstride * stride
     return outsize, totstride
 
-
-# def inFromOut(net, layernum):
-#     RF = 1
-#     for layer in reversed(range(layernum)):
-#         fsize, stride, pad = net[layer]
-#         RF = ((RF - 1) * stride) + fsize
-#     return RF
 def inFromOut(net, layernum):
     RF = 1
     for layer in reversed(range(layernum)):
